6cc07a90c6dcacc9b3af856c8baf0b8260d27f7d
zadekastrati1 <zk69847@ubt-uni.net>
2023-12-08 21:55:36 +0100

    Property details and slider added.

--- script.js
+++ script.js
@@ -42,3 +42,42 @@ function resetErrorElement(errorElement) {
     errorElement.innerHTML = "";
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    var imageUrls = [
+        "images/slide1.jpg",
+        "images/slide2.jpg",
+        "images/slide3.jpg",
+    ];
+
+    var sliderContainer = document.getElementById("image-slider");
+
+    imageUrls.forEach(function (imageUrl) {
+        var imgElement = document.createElement("img");
+        imgElement.src = imageUrl;
+        imgElement.classList.add("slider-image");
+        sliderContainer.appendChild(imgElement);
+    });
+
+    var currentIndex = 0;
+    var sliderImages = document.querySelectorAll(".slider-image");
+
+    function showImage(index) {
+        sliderImages.forEach(function (img, i) {
+            img.style.display = i === index ? "block" : "none";
+        });
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        showImage(currentIndex);
+    }
+
+    function prevImage() {
+        currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+        showImage(currentIndex);
+    }
+
+    showImage(currentIndex);
+    document.getElementById("nextBtn").addEventListener("click", nextImage);
+    document.getElementById("prevBtn").addEventListener("click", prevImage);
+});
